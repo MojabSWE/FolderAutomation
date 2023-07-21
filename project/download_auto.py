2fcbe599310f475f394500f046eeaf03adb6c6a3
MJ <mojab2002@gmail.com>
2023-07-22 00:12:50 +0300

    automated download folder
    automated the way any file i download, a folder is created for its extension and added to it. for example: gif, docx, pdf, etc..

---
+++ project/download_auto.py
@@ -0,0 +1,32 @@
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers.polling import PollingObserver
+
+import os
+import time
+
+watched_directory = "//Users/mojabjamal/Downloads"
+NEW_DIR = "gifs"
+
+
+class MyDownloadHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        for file_name in os.listdir(watched_directory):
+            watched_file = f"{watched_directory}/{file_name}"
+            new_dest_directory = f"{watched_directory}/{NEW_DIR}"
+
+            if not os.path.exists(new_dest_directory):
+                os.mkdir(new_dest_directory)
+
+            if file_name.endswith(".gif"):
+                os.rename(watched_file, f"{new_dest_directory}/{file_name}")
+
+if __name__ == "__main__":
+    event_handler = MyDownloadHandler()
+    observer = PollingObserver()
+    observer.schedule(
+        event_handler = event_handler, path=watched_directory, recursive = False
+    )
+
+    observer.start()
+    while True:
+        time.sleep(5)
